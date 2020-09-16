578781ef7dfdfc2cc99d0acdff5492a8403d7180
tttrrryyy789 <71361308+tttrrryyy789@users.noreply.github.com>
2020-09-16 08:53:10 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,10 @@ class LambdaDemo extends Component {
 
     return (
       <p>
-      filler
+        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
+        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
+        <br />
+        <span>{msg}</span>
       </p>
     )
   }
@@ -35,7 +38,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Bruh slowly im finding all of these
+            This is painful
           </p>
           <LambdaDemo />
         </header>
