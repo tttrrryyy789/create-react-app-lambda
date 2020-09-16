a853fa7a897ed287c7348b76c80bf9c548c549a5
tttrrryyy789 <71361308+tttrrryyy789@users.noreply.github.com>
2020-09-16 09:46:53 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ class LambdaDemo extends Component {
 
     return (
       <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
+        <button onClick={this.handleClick("Bruh")}>{loading ? "Loading..." : "Don't click"}</button>
+        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call joke creator"}</button>
         <br />
         <span>{msg}</span>
       </p>
