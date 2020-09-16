799ca9bfced785b62a25bf50d84674f1dbdae00d
tttrrryyy789 <71361308+tttrrryyy789@users.noreply.github.com>
2020-09-16 14:53:40 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          
+          <img src={logo} className="App-logo" alt="logo" />
           <p>
             This is Really Painful for Me Rn
           </p>
